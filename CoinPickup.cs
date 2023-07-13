7b92e8f5e7b2455d658d67c255d57859cf5f4463
lillybo <lillian.bojkov@gmail.com>
2023-07-13 21:00:26 +0300

    GDB-10 implementing coin pickup

---
+++ CoinPickup.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class CoinPickup : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D other) {
+        if (other.tag == "Player") {
+            Destroy(gameObject); 
+        }
+    }
+}
