63326702388715d660eac811d251a569e849ccbe
lillybo <lillian.bojkov@gmail.com>
2023-07-13 21:10:29 +0300

    GDB-10 adding coin SFX

--- CoinPickup.cs
+++ CoinPickup.cs
@@ -2,8 +2,12 @@ using UnityEngine;
 
 public class CoinPickup : MonoBehaviour
 {
+
+    [SerializeField] AudioClip coinPickuoSFX;
+
     void OnTriggerEnter2D(Collider2D other) {
         if (other.tag == "Player") {
+            AudioSource.PlayClipAtPoint(coinPickuoSFX, Camera.main.transform.position); 
             Destroy(gameObject); 
         }
     }
